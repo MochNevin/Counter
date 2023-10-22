90de56a70c929d7b1225609b806b6ebb7d508470
Nevin <qq2201591870@163.com>
2023-10-22 13:49:30 +0800

    update

--- Assets/Scripts/CounterBase.cs
+++ Assets/Scripts/CounterBase.cs
@@ -3,39 +3,109 @@ using System.Collections.Generic;
 using System.Data;
 using System.Globalization;
 using System.Text;
+using MySql.Data.MySqlClient;
 using TMPro;
 using UnityEngine;
 
 namespace Counter
 {
-    public readonly struct CounterData
-    {
-        public static HashSet<string> Signals = new()
-        {
-            "+", "-", "*", "/"
-        };
-    }
-
     public class CounterBase : MonoBehaviour
     {
+        private static MySqlConnection _connection;
         public TMP_Text resultText;
         public TMP_Text displayText;
+        public TMP_Text[] Histroy;
+        private Stack<string> _catchInput;
         private StringBuilder _currentInput;
         private DataTable _dataTable;
         private bool _hasEnter;
+        private int _count;
+        private List<string> _lastStrings;
 
         private void Awake()
         {
             Screen.SetResolution(800, 600, FullScreenMode.Windowed);
+            _lastStrings = new List<string>();
             _currentInput = new StringBuilder();
+            _catchInput = new Stack<string>();
             _dataTable = new DataTable();
+            _connection = GetConnection("localhost", "3306", "root", "abcd", "student");
+            _connection.Open();
         }
+
+        private void OnApplicationQuit()
+        {
+            if (_connection != null)
+            {
+                ClearString();
+                _connection.Close();
+            }
+
+            _connection = null;
+        }
+
+        public void PushString(string data) => InsertString(data);
+
+        public void ClearString()
+        {
+            const string query = "TRUNCATE TABLE my_strings";
+            using var command = new MySqlCommand(query, _connection);
+            command.ExecuteNonQuery();
+        }
+
+        private void InsertString(string data)
+        {
+            _count++;
+            const string query = "INSERT INTO my_strings (data) VALUES (@data)";
+            using var command = new MySqlCommand(query, _connection);
+            command.Parameters.AddWithValue("@data", data);
+            command.ExecuteNonQuery();
+        }
+
+        private string GetLastString()
+        {
+            const string query = "SELECT data FROM my_strings ORDER BY id DESC LIMIT @count";
+            using var command = new MySqlCommand(query, _connection);
+            using var reader = command.ExecuteReader();
+            if (reader.Read())
+                return reader.GetString("data");
+            return null;
+        }
+
+        public void UpdateStrings()
+        {
+            var strs = GetLastStrings(_count);
+            for (var i = 0; i < Math.Min(strs.Count, 10); ++i)
+                Histroy[i].text = i + 1 + ": " + strs[i];
+        }
+
+        public void Open(GameObject obj) => obj.SetActive(true);
+
+        public void Close(GameObject obj) => obj.SetActive(false);
+
+        public List<string> GetLastStrings(int count)
+        {
+            const string query = "SELECT data FROM my_strings ORDER BY id LIMIT @count";
+            using var command = new MySqlCommand(query, _connection);
+            command.Parameters.AddWithValue("@count", count);
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var data = reader.GetString("data");
+                _lastStrings.Insert(0, data);
+            }
+
+            return _lastStrings;
+        }
+
+        public static MySqlConnection GetConnection(string server, string port, string user, string password, string database) => new("Database=" + database + ";Data Source=" + server + ";User Id=" + user + ";Password=" + password + ";port=" + port + ";charset=utf8");
 
         public void OnButtonClick(string value)
         {
             if (_hasEnter)
                 OnClear();
             _currentInput.Append(value);
+            _catchInput.Push(value);
             displayText.text = _currentInput.ToString();
         }
 
@@ -43,18 +113,29 @@ namespace Counter
         {
             if (_hasEnter)
                 OnClear();
-            if (_currentInput.Length == 0)
+            if (_catchInput.Count == 0)
                 return;
-            _currentInput.Remove(_currentInput.Length - 1, 1);
+            var str = _catchInput.Pop();
+            _currentInput.Remove(_currentInput.Length - str.Length, str.Length);
             displayText.text = _currentInput.ToString();
+        }
+
+        public void OnAnswer()
+        {
+            var str = GetLastString();
+            if (str != null)
+                OnButtonClick(str);
         }
 
         public void OnCalculate()
         {
             try
             {
-                var result = Convert.ToDouble(_dataTable.Compute(_currentInput.ToString(), null));
-                resultText.text = result.ToString(CultureInfo.InvariantCulture);
+                var input = _currentInput.ToString();
+                var result = Convert.ToDouble(_dataTable.Compute(input, default));
+                var str = result.ToString(CultureInfo.InvariantCulture);
+                resultText.text = str;
+                PushString(str);
             }
             catch
             {
@@ -69,6 +150,7 @@ namespace Counter
         public void OnClear()
         {
             _currentInput.Clear();
+            _catchInput.Clear();
             resultText.text = "";
             displayText.text = "";
             _hasEnter = false;
